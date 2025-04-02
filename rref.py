269e56f365cc6a0c94faa2799226a957fbdec135
Dukarei <da00029@mix.wvu.edu>
2025-04-02 13:54:13 -0400

    uploading desktop verson pre-OS switch

---
+++ rref.py
@@ -0,0 +1,8 @@
+from sympy import *
+m = Matrix([
+        [.85, -.15, -.7, 0, 0],
+        [-.15, .8, -.4, -.25, 0],
+        [-.1, -.1, .45, -.25, 0],
+        [-.25, -.1, -.4, .75, 0]])
+M_rref = m.rref()
+print("The Row echelon form of matrix M and the pivot columns : {}".format(M_rref))
